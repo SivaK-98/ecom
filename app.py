7efd2a4a6272e5e4bdb2d20f5dc46bc48942afb8
SivaK-98 <hastthakanni@outlook.com>
2024-10-14 18:34:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, flash, redirect, render_template, request, url_for, session
 from bson import ObjectId
-from . import dbfile
+import dbfile
 from datetime import datetime
 from flask_bcrypt import Bcrypt
 from werkzeug.security import generate_password_hash, check_password_hash
